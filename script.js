199fb06cf7c1dfdbf21e330fc9336e14cc7a84b6
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2026-02-25 21:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
+const list = document.querySelector("#list");
 
+let itemCount = 0;
+
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    itemCount++;
+    const li = document.createElement("li");
+    li.textContent = "Item " + itemCount;
+    list.appendChild(li);
+  }
+
+  observer.observe(list.lastElementChild);
+}
+
+const observer = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    observer.unobserve(entries[0].target);
+    addItems(2);
+  }
+});
+
+// Add 10 items by default
+addItems(10);
